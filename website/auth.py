3b890ae11396f66fc8bd821b38b40d6020cb917a
Aakash Gangwar <aakashgangwar@Aakashs-MacBook-Air.local>
2025-03-02 00:37:50 +0530

    logging in multiple users

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
 
@@ -9,7 +9,22 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in Successfull!y', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again!', category='error')
+        else:
+            flash('Email does not exist!', category='error')         
+
     return render_template("login.html", boolean=True)
+
 
 @auth.route('logout')
 def logout():
@@ -23,7 +38,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email Already Exists!', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 char.', category='error')
         elif len(first_name) < 2:
             flash('Name must be greater than 2 char.', category='error')
